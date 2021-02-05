9d714b5597c3e6cb6e48da9d7620aba3c555613c
OlexiyTr <58363925+OlexiyTr@users.noreply.github.com>
2021-02-05 03:09:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/home_task_l20_version3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.home_task_l20_version3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
